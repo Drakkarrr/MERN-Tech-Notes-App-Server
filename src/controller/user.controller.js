7734c2e84274a0a1606aaa9bd208d12ee1d99f86
Drakkarrr <junrey404@gmail.com>
2023-06-10 11:22:05 +0800

    setup route for user

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,4 +4,22 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Auth user" })
 })
 
-export { authUser }
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Register user" })
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Logged Out successfully" })
+})
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "User Profile" })
+})
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Update User Profile" })
+})
+
+
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
+import { notFoundHandler, errorHandler } from "./middleware/error.middleware.js";
 
 dotenv.config();
 
@@ -11,12 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
 app.use("/api/users", userRoutes)
+// app.use('/api/products', productRoutes)
+
 app.get("/", (req, res) => {
     res.send("Server is now ready");
 }
 );
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
